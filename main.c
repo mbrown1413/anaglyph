96cf8261d0372e6406f7c60806da22cd3619495e
Michael Brown <mbrown1413@gmail.com>
2010-09-11 18:04:46 -0400

    Added saving image to files, and option parsing.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <cv.h>
 #include <highgui.h>
 
-#include "methods/interface.h"
+#include "methods/interface.h" // All method_* are defined here
 
 IplImage* combine_stereo(IplImage* left_eye, IplImage* right_eye, int offset_x)
 {
@@ -33,20 +33,60 @@ IplImage* combine_stereo(IplImage* left_eye, IplImage* right_eye, int offset_x)
     return stereo;
 }
 
+void usage_message(char* program_name) {
+        printf("Usage: %s [options] <left-eye> <right-eye>\n", program_name);
+        printf("The left-eye and right-eye arguments as of now, must be image files.  In the future, ffmpeg, avi and webcam support will be added.\n");
+        printf("\n");
+        printf("    -o <file_name>         Specify output file.  If none is specified, output\n                           will be shown in a window.\n");
+        exit(1);
+}
+
 int main(int argc, char** argv)
 {
     int offset_x = 127; // Added offset to left eye.  127 is center
     int previous_offset_x = -1;
-    cvNamedWindow("Stereo", CV_WINDOW_AUTOSIZE);
-    cvCreateTrackbar("x Offset", "Stereo", &offset_x, 255, NULL);
 
-    if (argc != 3) {
-        printf("Usage: %s <left-eye> <right-eye>\n", argv[0]);
-        printf("The two arguments as of now, must be image files.  In the future, ffmpeg, avi and webcam support will be added.\n");
-        exit(1);
+    // Parse Arguments
+    char* left_eye_string = NULL;
+    char* right_eye_string = NULL;
+    char* output_file = NULL;
+    for (int i=1; i<argc; i++) {
+
+        if (strcmp(argv[i], "-o") == 0) {
+            if (i == argc-1) {
+                printf("\nError: -o takes a filename as an argument.\n\n");
+                usage_message(argv[0]);
+            }
+            output_file = argv[i+1];
+            i++; // Skip next arg
+        } else if (strcmp(argv[i], "-h") == 0 ||
+                   strcmp(argv[i], "--help") == 0)
+        {
+            usage_message(argv[0]);
+        } else {
+            if (left_eye_string == NULL) {
+                left_eye_string = argv[i];
+            } else if (right_eye_string == NULL) {
+                right_eye_string = argv[i];
+            } else {
+                printf("\nError: Too many arguments specified.\n\n");
+                usage_message(argv[0]);
+            }
+        }
+
     }
-    IplImage* left_eye = cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR);
-    IplImage* right_eye = cvLoadImage(argv[2], CV_LOAD_IMAGE_COLOR);
+    if (!left_eye_string || !right_eye_string) {
+        printf("\nError: Left and Right eye images not specified.\n\n");
+        usage_message(argv[0]);
+    }
+
+    if (output_file == NULL) {
+        cvNamedWindow("Stereo", CV_WINDOW_AUTOSIZE);
+        cvCreateTrackbar("x Offset", "Stereo", &offset_x, 255, NULL);
+    }
+
+    IplImage* left_eye = cvLoadImage(left_eye_string, CV_LOAD_IMAGE_COLOR);
+    IplImage* right_eye = cvLoadImage(right_eye_string, CV_LOAD_IMAGE_COLOR);
     IplImage* stereo = NULL;
 
     if (left_eye == NULL) {
@@ -70,19 +110,24 @@ int main(int argc, char** argv)
 
         time_start = clock();
 
-        //if (offset_x != previous_offset_x)
+        if (offset_x != previous_offset_x)
         {
             cvFree(&stereo); // Doesn't do anything the first time through
             stereo = combine_stereo(left_eye, right_eye, offset_x-127);
-            cvShowImage("Stereo", stereo);
+            if (output_file == NULL) {
+                cvShowImage("Stereo", stereo);
+            } else {
+                cvSaveImage(output_file, stereo, 0);
+                exit(0);
+            }
             previous_offset_x = offset_x;
         }
 
         time_end = clock();
         total_elapsed += (float) (time_end-time_start)/CLOCKS_PER_SEC;
-        printf("Total fps: %f\n", frames/total_elapsed);
+        //printf("Total fps: %f\n", frames/total_elapsed);
 
-        key = cvWaitKey(100);
+        key = cvWaitKey(-1);
         if ( (char) key == 27) { // Esc to exit
             break;
         }
