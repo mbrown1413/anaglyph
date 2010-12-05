0a5388ba1e45842d75a1a849ce6fc368a544f845
dkliban <dkliban@dhcp14.oscar.ncsu.edu>
2010-12-05 17:17:36 -0500

    Changed utils so image tasks are dispatched.  Changed anaglyph task to return params.

--- celery/tasks.py
+++ celery/tasks.py
@@ -3,14 +3,10 @@ import shlex, subprocess
 
 @task
 def McAllisterAnaglyphTask(**kwargs):
-    #print 'handling task: Frame %(0)s, of %(combined_prefix)s' % kwargs
     args = ["/home/dkliban/anaglyph/bin/cielab", kwargs["left_image"], kwargs["right_image"], "-o", kwargs["combined_image"]]
-    try:
-        p = subprocess.Popen(args)
-        subprocess.Popen.wait(p)
-        return 1
-    except Exception:
-        return 0
+    p = subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
+    print args
+    return kwargs
 
 @task
 def McAllisterVideoTask(**kwargs):
--- celery/utils.py
+++ celery/utils.py
@@ -6,6 +6,24 @@ import sys
 
 sys.path.append("/usr/local/lib/python2.6/site-packages")
 
+def testing(**params):
+    CV_CAP_PROP_FRAME_COUNT = 7
+
+    #"/home/michael/anaglyph/sw-L/left.avi"
+
+    video = cv.CaptureFromFile(params['left_video'])
+    print params['left_video']
+    number_frames = cv.GetCaptureProperty(video, cv.CV_CAP_PROP_FRAME_COUNT)
+
+    print number_frames
+
+    width = int(cv.GetCaptureProperty(video, cv.CV_CAP_PROP_FRAME_WIDTH))
+    height = int(cv.GetCaptureProperty(video, cv.CV_CAP_PROP_FRAME_HEIGHT))
+
+    for i in xrange(number_frames):
+        frame = cv.QueryFrame(video)
+        cv.SaveImage("%(name)s/%(fn)s.jpg" % {'name':params['combined_name'], 'fn':i}, frame)
+
 
 def dispatch_video(**params):
     CV_CAP_PROP_FRAME_COUNT = 7
@@ -21,24 +39,30 @@ def dispatch_video(**params):
     width = int(cv.GetCaptureProperty(video, cv.CV_CAP_PROP_FRAME_WIDTH))
     height = int(cv.GetCaptureProperty(video, cv.CV_CAP_PROP_FRAME_HEIGHT))
 
+    for i in xrange(200):
+        frame = cv.QueryFrame(video)
+        cv.SaveImage("%(name)s/%(fn)s.jpg" % {'name':params['combined_name'], 'fn':i}, frame)
+    
     output = cv.CreateVideoWriter(params['combined_name']+".avi", cv.FOURCC('M','J','P','G'), 30, (width/2, height), 1)
     the_subtasks = []
-    results = {}
-    #for i in xrange(left_number_frames):
-    for i in xrange(2500):
-        results[i] = McAllisterVideoTask.delay(left_video=params['left_video'],right_video="-s",frame=str(i),combined_name=params['combined_name'])
-    
-    current = 0
-    tt = 0
-    while (results):
-        tt = tt + 1
-        print tt
-        if (results[current].ready()):
-            task_result = results.pop(current).result
-            print '%(combined_name)s%(frame)s.png' % task_result
-            next_frame = cv.LoadImage('%(combined_name)s%(frame)s.png' % task_result)
-            cv.WriteFrame(output, next_frame)
-            current = current + 1
+    for i in xrange(200):
+        the_subtasks.append(McAllisterAnaglyphTask.subtask(left_image="%(name)s/%(fn)s.jpg" % {'name':params['combined_name'], 'fn':i},right_image="-s",combined_image="%(name)s/%(fn)sC.jpg" % {'name':params['combined_name'], 'fn':i}))
+    job = TaskSet(tasks=the_subtasks)
+    results = job.apply_async()
+
+    for r in results.iterate():
+        next_frame = cv.LoadImage('%(combined_image)s' % r)
+        print r
+        cv.WriteFrame(output, next_frame)
+
+    #current = 0
+    #while (results):
+    #    if (results[0].ready()):
+    #        task_result = results.popleft().result
+    #        print '%(combined_name)s%(frame)s.png' % task_result
+    #        next_frame = cv.LoadImage('%(combined_name)s%(frame)s.png' % task_result)
+    #        cv.WriteFrame(output, next_frame)
+    #        current = current + 1
 
         
 def test_image():
@@ -46,6 +70,9 @@ def test_image():
 
 def test_video():
     dispatch_video(left_video="/home/dkliban/anaglyph/videos/Ray3D.avi", right_video="-s", combined_name="/home/dkliban/dkliban_research/storage/ray3Danaglyph")
+
+def test_testing():
+    testing(left_video="/home/dkliban/anaglyph/videos/Ray3D.avi", right_video="-s",          combined_name="/home/dkliban/dkliban_research/storage/ray3Danaglyph")
 
 def dispatch_image(**params):
     the_subtasks = []
