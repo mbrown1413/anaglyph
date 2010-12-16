5a78bf3a5e5258edf13db77152203b51dea6bb71
dkliban <dkliban@dhcp14.oscar.ncsu.edu>
2010-12-16 15:54:13 -0500

    Modified memcache task. Now can provide several frames per task.

--- celery/tasks.py
+++ celery/tasks.py
@@ -6,13 +6,18 @@ import memcache
 @task
 def MemcacheAnaglyphTask(**kwargs):
     mc = memcache.Client(['152.14.17.125:11211'], debug=1)
-    frame = mc.get("%(frame)s" % kwargs)
-    cv_im = cv.CreateImage(kwargs['size'], cv.IPL_DEPTH_8U, 3)
-    cv.SetData(cv_im, frame, kwargs['size'][0]*3)
-    cv.SaveImage("/home/dkliban/anaglyph/images/tmp.bmp", cv_im)
-    args = ["/home/dkliban/anaglyph/bin/cielab", "/home/dkliban/anaglyph/images/tmp.bmp", kwargs["right_image"], "-o", kwargs["combined_image"]]
-    p = subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
-    print args
+    frames_per_task = kwargs['frames_per_task']
+    start_frame = int(kwargs['frame'])
+    for i in xrange(frames_per_task):
+        frame_id = (start_frame + i).__str__()
+        frame = mc.get(frame_id)
+        cv_im = cv.CreateImage(kwargs['size'], cv.IPL_DEPTH_8U, 3)
+        cv.SetData(cv_im, frame, kwargs['size'][0]*3)
+        cv.SaveImage("/home/dkliban/anaglyph/images/tmp.bmp", cv_im)
+        combined_image = kwargs['combined_image'] + frame_id + "C.bmp"
+        args = ["/home/dkliban/anaglyph/bin/cielab", "/home/dkliban/anaglyph/images/tmp.bmp", kwargs["right_image"], "-o", combined_image]
+        p = subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
+        print args
     mc.disconnect_all()
     return kwargs
 
